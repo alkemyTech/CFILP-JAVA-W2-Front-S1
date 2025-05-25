17cf0f50ded525ce3fe22f2216d611b3247949b7
teshy18 <sebas.tesitore@gmail.com>
2025-05-25 02:58:49 -0300

    Refactor: Se actualiza el client.html para cargar de forma estatica los archivos js correspondientes a los modales. Se deshabilita   index.js

--- dashboard/client/components/modals/index.js
+++ dashboard/client/components/modals/index.js
@@ -1,43 +1,48 @@
-/**
- * Archivo principal para cargar todos los módulos de modales
- * Este archivo debe ser incluido después de modal-manager.js
- */
 
-// Importar todos los manejadores de modales
-document.addEventListener("DOMContentLoaded", () => {
-    // Verificar que el modal manager esté disponible
-    if (!window.modalManager) {
-        console.error("Modal Manager no está disponible. Asegúrate de cargar modal-manager.js primero.")
-        return
-    }
+//Este archivo no se esta cargando bien, por lo que no se esta ejecutando el código de los modales
+// Se procede a cargar los archivos de los modales directamente en el cliente.html
+// 
 
-    // Cargar los scripts de los manejadores de modales
-    //todo: agregar el modal "./components/modals/account.js",
-    const modalScripts = [
-      // "./components/modals/createAccountModal.js",
-        "./components/modals/deposit.js",
-        "./components/modals/withdrawal.js",
-        "./components/modals/transfer.js",
-        "./components/modals/confirmationModal.js",
-    ]
+// /**
+//  * Archivo principal para cargar todos los módulos de modales
+//  * Este archivo debe ser incluido después de modal-manager.js
+//  */
 
-    // Función para cargar un script
-    function loadScript(src) {
-        return new Promise((resolve, reject) => {
-            const script = document.createElement("script")
-            script.src = src
-            script.onload = resolve
-            script.onerror = reject
-            document.body.appendChild(script)
-        })
-    }
-    // Cargar todos los scripts en secuencia
-    modalScripts
-        .reduce((promise, scriptSrc) => promise.then(() => loadScript(scriptSrc)), Promise.resolve())
-    .then(() => {
-        console.log("Todos los manejadores de modales han sido cargados")
-    })
-    .catch((error) => {
-        console.error("Error al cargar los manejadores de modales:", error)
-    })
-})
+// // Importar todos los manejadores de modales
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Verificar que el modal manager esté disponible
+//     if (!window.modalManager) {
+//         console.error("Modal Manager no está disponible. Asegúrate de cargar modal-manager.js primero.")
+//         return
+//     }
+
+//     // Cargar los scripts de los manejadores de modales
+//     //todo: agregar el modal "./components/modals/account.js",
+//     const modalScripts = [
+//       // "./components/modals/createAccountModal.js",
+//         "./components/modals/deposit.js",
+//         "./components/modals/withdrawal.js",
+//         "./components/modals/transfer.js",
+//         "./components/modals/confirmationModal.js",
+//     ]
+
+//     // Función para cargar un script
+//     function loadScript(src) {
+//         return new Promise((resolve, reject) => {
+//             const script = document.createElement("script")
+//             script.src = src
+//             script.onload = resolve
+//             script.onerror = reject
+//             document.body.appendChild(script)
+//         })
+//     }
+//     // Cargar todos los scripts en secuencia
+//     modalScripts
+//         .reduce((promise, scriptSrc) => promise.then(() => loadScript(scriptSrc)), Promise.resolve())
+//     .then(() => {
+//         console.log("Todos los manejadores de modales han sido cargados")
+//     })
+//     .catch((error) => {
+//         console.error("Error al cargar los manejadores de modales:", error)
+//     })
+// })
