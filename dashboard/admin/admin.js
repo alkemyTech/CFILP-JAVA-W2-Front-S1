da9e9e7f893afcbb5d37d52059f86c7e301394ee
teshy18 <sebas.tesitore@gmail.com>
2025-05-28 01:21:55 -0300

    feat(admin): add admin access button and role verification

--- dashboard/admin/admin.js
+++ dashboard/admin/admin.js
@@ -13,6 +13,16 @@ document.addEventListener('DOMContentLoaded', function () {
         window.location.href = "./../../index.html";
         return;
     }
+
+       // Verificar que el usuario sea ADMIN
+    const userData = JSON.parse(localStorage.getItem('data'));
+    if (!userData || !userData.roles || !userData.roles.includes("ADMIN")) {
+        alert("Acceso denegado. No tienes permisos de administrador.");
+        window.location.href = "./../../index.html";
+        return;
+    }
+
+
     // Configurar acciones para el boton de cerrar sesión
     const logoutBtn = document.getElementById("logoutBtn");
     if (logoutBtn) {
